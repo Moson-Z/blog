5b0eeba9d4e4803eb5274a33f4d979d6adfdc3ed
moson <514939920@qq.com>
2020-04-18 00:48:48 +0800

    原功能修改为类方法，添加RSS功能

---
+++ blogproject/settings/local.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.6+
+# -*- coding:utf-8 -*-
+from .common import *
+
+SECRET_KEY = 'development-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
---
+++ blogproject/settings/production.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.6+
+# -*- coding:utf-8 -*-
+from .common import *
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = False
+ALLOWED_HOSTS = ['www.moson.vip']
